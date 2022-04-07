e00e59a251292cdc767a7fc90d4056fcb963cfc7
Paul <paulnguyen.mn@gmail.com>
2022-04-07 12:04:53 +0700

    20-08 do not mutate props

--- src/features/labs/Student.tsx
+++ src/features/labs/Student.tsx
@@ -5,11 +5,22 @@ export interface StudentCardProps {
   student: Student
 }
 
+// Props are READ ONLY
+// DO NOT MUTATE Props
+// Props are immutable
 export function StudentCard({ student }: StudentCardProps) {
-  const { name, isHero } = student
+  let { name, isHero } = student
+
+  // name = 'Bob'
+  function handleClick() {
+    student.name = 'Bob'
+    console.log(student)
+    // - not trigger re-render
+    // - inconsistent data
+  }
 
   return (
-    <div>
+    <div onClick={handleClick}>
       Student: {name} {isHero ? 'hero' : 'no-hero'}
     </div>
   )
