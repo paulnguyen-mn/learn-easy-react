9947c62b1c3f4a2c1c7c0ba32605762a7eaa3fec
Paul <paulnguyen.mn@gmail.com>
2022-04-30 14:35:42 +0700

    Revert "20-10 props.children"
    This reverts commit ee0410256bc0af0c1ccd8ed3d52e74ce3bd9829f.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 import { Footer, Header } from './components/common'
-import { MyText } from './features/labs/MyText'
 import { StudentCard } from './features/labs/Student'
 import { Student } from './models/student'
 
@@ -24,21 +23,6 @@ function App() {
       <Header></Header>
       <StudentCard student={john} onClick={handleStudentClick} />
       <Footer></Footer>
-
-      <MyText>hello world</MyText>
-      <MyText>{123}</MyText>
-      <MyText>{null}</MyText>
-      <MyText>{undefined}</MyText>
-      <MyText>{true}</MyText>
-
-      <MyText>
-        <div>hello div</div>
-      </MyText>
-
-      <MyText>
-        <div>hello div 1</div>
-        <div>hello div 2</div>
-      </MyText>
     </div>
   )
 }
--- src/features/labs/MyText.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactChild, ReactElement, ReactNode } from 'react'
-
-export interface MyTextProps {
-  children: ReactNode
-}
-
-export function MyText({ children }: MyTextProps) {
-  return <div>{children}</div>
-}
