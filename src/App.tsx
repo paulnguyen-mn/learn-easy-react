edb3c6057c8b4195ec7c6cdaf907fb62bb5e968f
Paul <paulnguyen.mn@gmail.com>
2022-05-02 16:01:01 +0700

    21-05 setState is async

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 
 function App() {
+  let i = 0
+
   const [showMore, setShowMore] = useState(false)
   const [count, setCount] = useState(0)
   const [name, setName] = useState('')
 
   function handleIncreaseClick() {
-    setCount(count + 1)
+    const newCount = count + 1
+    setCount(count + 1) // asyn func
+    console.log('after setting count', newCount) // old count
+    i += 5
   }
+
+  console.log({ count, i })
 
   return (
     <div>
