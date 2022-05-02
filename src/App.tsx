a070d3457d11fd891c64de8006f1904b9701e350
Paul <paulnguyen.mn@gmail.com>
2022-05-02 16:40:07 +0700

    21-06 useState initializer

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import { useState } from 'react'
 
+// pure
+// impure
+let run = 0
+function calcCount() {
+  run += 5
+  console.log('calc count', run)
+  return run
+}
+
 function App() {
   let i = 0
 
   const [showMore, setShowMore] = useState(false)
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(calcCount)
   const [name, setName] = useState('')
 
   function handleIncreaseClick() {
     const newCount = count + 1
     setCount(count + 1) // asyn func
-    console.log('after setting count', newCount) // old count
+    // console.log('after setting count', newCount) // old count
     i += 5
   }
 
